40860f1799793b95cc6f9ec32a054b648f5bc253
Herbert Ssevume <you@example.com>
2020-09-01 00:05:31 +0100

    version 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,7 +33,7 @@ class Home extends Component {
                   <h5>Full Stack Web Developer</h5>
                   <blockquote className="blockquote">
                   <p className="left-align">I am a freelance front-end and back-end web developer based in Basingstoke UK. 
-                  Building user-friendly websites and applications is truly a passion on mine. In addition to my knowledge base,
+                  Building user-friendly websites and applications is truly a passion of mine. In addition to my knowledge base,
                    I actively seek out new technologies and stay up-to-date on industry trends and advancements.</p>
                   </blockquote>
                 </div>
